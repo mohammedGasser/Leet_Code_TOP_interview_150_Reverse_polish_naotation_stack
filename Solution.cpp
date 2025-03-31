becdfd9bd5fb9de1e0da20c4657235d2f0f59870
mohammedGasser <mohamedgassermohamede@gmail.com>
2025-03-31 02:21:11 +0200

    Update Solution.cpp

--- Solution.cpp
+++ Solution.cpp
@@ -1,4 +1,32 @@
+
 class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> st;
+        for(string s: tokens){
+             if(s=="+" || s=="-" || s=="/" || s=="*"){
+                int x = st.top();
+                st.pop();
+                int y = st.top();
+                st.pop();
+                int ans;
+                  if(s=="+")ans=y+x;
+                  else if(s=="*") ans=y*x;
+                  else if(s=="-")  ans=y-x;
+                  else if(s=="/")  ans=y/x;
+                      
+                st.push(ans);
+            }
+            else{
+                st.push(stoi(s));
+            }
+        }
+        return st.top();
+    }
+};
+
+
+class Another_Solution {
 public:
     int evalRPN(vector<string>& tokens) {
         stack<int> st  ;
